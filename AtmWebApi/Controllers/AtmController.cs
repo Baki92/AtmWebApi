98d335528a2f3c2f77ec94f31a33ae17979247bc
Baki92 <bakiprog@gmail.com>
2021-11-04 17:46:25 +0100

    Create new project

---
+++ AtmWebApi/Controllers/AtmController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AtmWebApi.Controllers
+{
+    [Route("api")]
+    [ApiController]
+    public class AtmController : ControllerBase
+    {
+        [HttpPost]
+        [Route("withdrawal")]
+        public void Withdrawal()
+        {
+
+        }
+        [HttpPost]
+        [Route("deposit")]
+        public void Deposit(int amount)
+        {
+
+        }
+    }
+}
